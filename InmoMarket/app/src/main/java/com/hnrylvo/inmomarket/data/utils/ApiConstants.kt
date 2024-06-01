fdcd2d670e4e4290c6f0a94e97d980ec59366f05
Henry <00037322@uca.edu.sv>
2024-06-01 00:02:49 -0600

    Fix: BASE_URL 2.0

--- InmoMarket/app/src/main/java/com/hnrylvo/inmomarket/data/utils/ApiConstants.kt
+++ InmoMarket/app/src/main/java/com/hnrylvo/inmomarket/data/utils/ApiConstants.kt
@@ -1,7 +1,7 @@
 package com.hnrylvo.inmomarket.data.utils
 
 object ApiConstants {
-    const val BASE_URL = "https://localhost:3000/api/"
+    const val BASE_URL = "http://10.0.2.2:8080/api/"
 
     const val LOGIN_PATH = "login"
 }
