227607e79b12ab6fdfccfd57b84029b84ced7179
Josue Zelada <00053722@uca.edu.sv>
2024-06-24 11:37:14 -0600

    ADD: auth modification

--- backEndProject/src/middleware/validateToken.js
+++ backEndProject/src/middleware/validateToken.js
@@ -3,21 +3,15 @@ import {SECRETE_KEY} from '../config.js'
 
 
 export const authRequired = (req, res, next) => {
-  const { token } = req.cookies;
+  const token = req.cookies.token || req.headers['authorization'];
 
-  if (!token)
-    return res
-      .status(401)
-      .json({ message: "No token used, authorization denied" });
-  
-      jwt.verify(token, SECRETE_KEY, (err, user)=> {
+  if (!token) {
+    return res.status(401).json({ message: "No token used, authorization denied" });
+  }
 
-        if (err) return res.status(404).json({message: "Invalid token"});
-        req.user = user;
-        
-        next();
-
-      });
-
-  
+  jwt.verify(token, SECRETE_KEY, (err, user) => {
+    if (err) return res.status(404).json({ message: "Invalid token" });
+    req.user = user;
+    next();
+  });
 };
